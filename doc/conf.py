b266972e2ad772a53f931c01f4079af4289196dd
Richard Magala <105811040+MAGALA-RICHARD@users.noreply.github.com>
2025-04-04 15:09:47 -0500

    Add files via upload

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,46 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
-
+project = 'apsimNGpy'
+copyright = '2025, richard magala'
+author = 'richard magala'
+release = '0.32'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['sphinx.ext.duration',
+'sphinx.ext.napoleon',
+                    'sphinx.ext.doctest',
+                    'sphinx.ext.autodoc',
+                    'sphinx.ext.autosummary',
+                    "sphinx.ext.intersphinx",
+                    ]
 
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+autodoc_mock_imports = []  # Add external modules to mock if they are not installed
 
+autosummary_generate = True  # Automatically create stub pages for modules
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", '.apsimx', '.db']
+
 html_static_path = ['_static']
+
+import os, sys
+sys.path.insert(0, os.path.abspath('../'))
--- doc/index.rst
+++ doc/index.rst
@@ -1,14 +1,37 @@
-.. Example documentation master file, created by
-   sphinx-quickstart on Sat Sep 23 20:35:12 2023.
+.. apsimNGpy documentation master file, created by
+   sphinx-quickstart on Sun Mar 30 22:19:37 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Example's documentation!
-===================================
+apsimNGpy Documentation
+=======================
+
+apsimNGpy: The Next-Generation Agro-ecosystem Simulation Library
+================================================================
+
+apsimNGpy is a cutting-edge, open-source framework designed for advanced agroecosystem modeling.
+Built on object-oriented principles, it provides:
+
+- Fast batch file simulation
+- Model prediction and evaluation
+- Seamless APSIMX file editing
+- Weather data retrieval
+- Efficient soil profile development
+
+.. automodule:: apsimNGpy
+    :members:
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 4
+   :caption: Table of Contents
 
-   some-feature.md
-   another-feature.md
+   Home <self>
+   getting started
+   Usage
+   model inspection
+   cli
+   factorial_experiments
+   api
+
+
+
